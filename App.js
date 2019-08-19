59804daa193101940b01857b5adf48c1e0339ca9
Harikrishnan R <harikrishnan@hubspire.com>
2019-08-19 11:25:38 +0530

    Header

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ import SignUp from './CommonPages/Containers/SignUp';
 import OrderBundle from './CommonPages/Containers/OrderBundle';
 
 import Dashboard from './Client/Containers/Dashboard';
+import Header from './Client/Components/Header';
 
 class App extends Component{
 	constructor(props){
@@ -39,6 +40,7 @@ class App extends Component{
    		
    			route=(
               <div>
+                 <Header/>
            			 <Route exact path="/" component={Dashboard} />
               </div>
    				)
