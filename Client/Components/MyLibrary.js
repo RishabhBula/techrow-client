c6f6bacf10fd8fe13de9361be59c3174039a111f
Harikrishnan R <harikrishnan@hubspire.com>
2019-09-20 12:04:33 +0530

    library modified with dummy content

--- Client/Components/MyLibrary.js
+++ Client/Components/MyLibrary.js
@@ -13,12 +13,23 @@ class MyLibrary extends Component{
 	constructor(props){
 		super(props);
     this.state = {
-      
+      data :[
+              {id:"123",shortdisc:"Lorem Ipsum Sit Dolor - amet Specialization",longdisc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium"},
+              {id:"456",shortdisc:"Lorem Ipsum Sit Dolor - amet Specialization",longdisc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium"},
+              {id:"789",shortdisc:"Lorem Ipsum Sit Dolor - amet Specialization",longdisc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium"},
+              {id:"987",shortdisc:"Lorem Ipsum Sit Dolor - amet Specialization",longdisc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium"},
+              {id:"654",shortdisc:"Lorem Ipsum Sit Dolor - amet Specialization",longdisc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium"},
+              {id:"321",shortdisc:"Lorem Ipsum Sit Dolor - amet Specialization",longdisc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium"},
+            ]
     }
 	}
   
   componentDidMount(){
       
+  }
+
+  onItemClick(id){
+    window.location.href='#/class/:'+id+'/:theater'
   }
 
 
@@ -33,78 +44,18 @@ class MyLibrary extends Component{
             </div>
             <div className="classesList">
               <div className="row">
-                <div className="col-lg-3 col-md-4">
-                  <a href="#/class/:542321122/:theater" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
-                <div className="col-lg-3 col-md-4">
-                  <a href="#" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
-                <div className="col-lg-3 col-md-4">
-                  <a href="#" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
-                <div className="col-lg-3 col-md-4">
-                  <a href="#" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
-                <div className="col-lg-3 col-md-4">
-                  <a href="#" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
-                <div className="col-lg-3 col-md-4">
-                  <a href="#" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
-                <div className="col-lg-3 col-md-4">
-                  <a href="#" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
-                <div className="col-lg-3 col-md-4">
-                  <a href="#" className="each-class">
-                    <div className="class-banne-wrap">
-                      <img src="../images/class-image.jpg" className="img-fluid class-banner" />
-                    </div>
-                    <h3>Lorem Ipsum Sit Dolor - amet Specialization</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
-                  </a>
-                </div>
+                {this.state.data.map((item,index) =>{
+                  return(
+                    <div className="col-lg-3 col-md-4">
+                      <a onClick={() =>{ this.onItemClick(item.id) }} className="each-class">
+                        <div className="class-banne-wrap">
+                          <img src="../images/class-image.jpg" className="img-fluid class-banner" />
+                        </div>
+                        <h3>{item.shortdisc}</h3>
+                        <p>{item.longdisc}</p>
+                      </a>
+                    </div>)
+                })}
                 
 
               </div>
