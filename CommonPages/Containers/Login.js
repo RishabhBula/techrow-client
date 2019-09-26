13c730524172aa2b45ce22674283eb78fa5999b3
Harikrishnan R <harikrishnan@hubspire.com>
2019-09-26 13:33:22 +0530

    custom action handler

--- CommonPages/Containers/Login.js
+++ CommonPages/Containers/Login.js
@@ -62,16 +62,10 @@ class Login extends Component{
       if(this.state.email==""){
         this.setState({error:true,errortext:"Please enter email to continue"})
       }else{
-        var actionCodeSettings = {
-          // After password reset, the user will be give the ability to go back
-          // to this page.
-          url: 'https://hk-project-0.firebaseapp.com/#/login',
-          handleCodeInApp: false
-        };
-        firebase.auth().sendPasswordResetEmail(email.trim().toLocaleLowerCase(),actionCodeSettings)
+        firebase.auth().sendPasswordResetEmail(email.trim().toLocaleLowerCase())
         .then((res) =>{
           console.log("forgot success response",res)
-          Notification("success","Email sent","Reset password link sent to your account")
+          Notification("success","Email sent","Reset password link sent to your email")
           this.setState({forgotstate:false,email:"",password:""})
           // Email sent.
         })
