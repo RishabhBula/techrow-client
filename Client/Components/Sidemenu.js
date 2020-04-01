6cf98f74b4c853eb1c1acc71ecaf40eb5d40064a
A-Mozeak <alex@astar.app>
2020-04-01 14:02:03 -0400

    Removed test script, refactored LW API calls.

--- Client/Components/Sidemenu.js
+++ Client/Components/Sidemenu.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { setClassMode } from '../../actions/setClassMode';
 
+const CLIENT_ID = '5e7531d798cabfc868194676';
+const CLIENT_SECRET = 'dKJsJmhDoJVHNioweMFR8tLVzMGU4So6auqxfBBqib8ojfpZBW';
 
 class Sidemenu extends Component {
   constructor(props) {
@@ -14,7 +16,6 @@ class Sidemenu extends Component {
   }
 
   componentDidMount() {
-    console.log("Sidebar Mounted: ", this); // Pass in SSO username here
 
   }
 
@@ -24,31 +25,40 @@ class Sidemenu extends Component {
   }
 
   async lwLogin() {
+    // Need to validate whether or not user has a LW account, need to pass in new username if not.
+    // For now users will sign UP for the academy separately, then be able to use single sign on.
+    // We should also proxy the client_id and secret
     let email = this.props.userData.email;
+
+    let lwToken = await this.getLWToken();
+    return await this.performLWSignOn(email, lwToken);
+  }
+
+  async getLWToken() {
     const tokenRequest = await fetch("https://api.learnworlds.com/oauth2/access_token", {
-      body: "data={\"client_id\":\"5e7531d798cabfc868194676\",\"client_secret\":\"dKJsJmhDoJVHNioweMFR8tLVzMGU4So6auqxfBBqib8ojfpZBW\",\"grant_type\":\"client_credentials\"}",
+      body: `data={\"client_id\":\"${CLIENT_ID}\",\"client_secret\":\"${CLIENT_SECRET}\",\"grant_type\":\"client_credentials\"}`,
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        "Lw-Client": "5e7531d798cabfc868194676"
+        "Lw-Client": `${CLIENT_ID}`
       },
       method: "POST"
     });
     const tokenResponse = await tokenRequest.json();
-    const lwToken = tokenResponse.tokenData.access_token;
+    return tokenResponse.tokenData.access_token;
+  }
 
+  async performLWSignOn(email, token) {
     const ssoRequest = await fetch("https://api.learnworlds.com/sso", {
-      body: "data={\"email\":\"info@techrow.org\", \"redirectUrl\":\"https://techrowacademy.org\"}",
+      body: `data={\"email\":\"${email}\", \"redirectUrl\":\"https://www.techrowacademy.org/start\"}`,
       headers: {
-        "Authorization": "Bearer " + lwToken,
+        "Authorization": "Bearer " + token,
         "Content-Type": "application/x-www-form-urlencoded",
-        "Lw-Client": "5e7531d798cabfc868194676"
+        "Lw-Client": `${CLIENT_ID}`
       },
       method: "POST"
     })
     const ssoResponse = await ssoRequest.json();
-    const ssoURL = ssoResponse.url;
-
-    console.log("Login received: ", ssoURL);
+    return ssoResponse.url;
   }
 
   render() {
@@ -57,7 +67,6 @@ class Sidemenu extends Component {
         {this.props.classMode.route == "" && (<div className="sidemenu-group">
           <a className="ative">My Library</a>
           <a href="#/marketplace">Marketplace</a>
-          <button onClick={() => { this.lwLogin() }}>LearnWorlds</button>
         </div>)}
         {this.props.classMode.route == "class" && (
           <div className="sidemenu-group">
